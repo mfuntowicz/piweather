7a02a1ff74ad44c7f039bdad3900ec06ac746a81
Morgan Funtowicz <funtowiczmo@gmail.com>
2024-08-21 21:30:17 +0000

    (feat) completely remove tokio dependency

--- piweather-agent/src/main.rs
+++ piweather-agent/src/main.rs
@@ -4,58 +4,74 @@ use piweather_agent::sensors::{Am2315, Sensor};
 use piweather_common::errors::PiWeatherError;
 use piweather_common::Payload;
 use std::path::PathBuf;
-use std::time::Instant;
-use tokio::sync::mpsc::{channel, Receiver};
-use tracing::{debug, error, info};
+use std::sync::mpsc::{channel, Sender};
+use std::thread;
+use std::thread::sleep;
+use std::time::{Duration, Instant};
+use tracing::{error, info};
 use tracing_subscriber;
 
 #[derive(Debug, Parser)]
 #[command(version, about, long_about = None)]
 struct Args {
-    #[arg(
-        long,
-        default_value = "16",
-        help = "Maximum number of queued readouts before blocking"
-    )]
-    backlog: usize,
-
     #[arg(short, long, help = "The I2C device to use")]
     bus: PathBuf,
+
+    #[arg(
+        short,
+        long,
+        default_value = "10",
+        help = "Interval between two sensor readings"
+    )]
+    frequency: usize,
 
     #[arg(required = true, help = "URI where to push the readouts")]
     destination: String,
 }
 
-async fn weather_readouts_scheduler<T>(
+fn weather_readouts_driver<T: I2CDeviceFactory>(
     factory: T,
-    mut readouts: Receiver<Payload<3>>,
-) -> Result<usize, PiWeatherError>
-where
-    T: I2CDeviceFactory,
-{
+    frequency: Duration,
+    pipe: Sender<Payload>,
+) -> Result<usize, PiWeatherError> {
     // Initiate sensors
     let mut am2315 = Am2315::with_i2c_factory(factory)?;
 
     // Count the number of message sent
     let messages = 0usize;
+
     loop {
-        match readouts.recv().await {
-            Some(payload) => {
-                debug!("Received payload: {:?}", payload);
+        match am2315.read() {
+            Ok(readouts) => {
+                if let Some(readouts) = readouts {
+                    info!("Am2315: {:?}", &readouts);
+                    let modalities = [readouts[0].into(), readouts[1].into()];
+                    if let Err(err) = pipe.send(Payload::now(&modalities)) {
+                        error!(
+                            device = "am2315",
+                            error = err.to_string(),
+                            "Failed to send through the pipe"
+                        );
+                        break;
+                    }
+                }
             }
-            None => {
-                debug!("Received termination from the channel");
-                break;
+            Err(err) => {
+                error!(
+                    device = "am2315",
+                    error = err.to_string(),
+                    "I/O Error on AM2315"
+                )
             }
         }
+        sleep(frequency);
     }
 
     info!("Scheduler exiting");
     Ok(messages)
 }
 
-#[tokio::main(flavor = "current_thread")]
-async fn main() -> Result<(), PiWeatherError> {
+fn main() -> Result<(), PiWeatherError> {
     tracing_subscriber::fmt::init();
 
     // Parse arguments
@@ -63,14 +79,16 @@ async fn main() -> Result<(), PiWeatherError> {
     let boot = Instant::now();
 
     // Create the I2C bus from the provided file address
-    let factory = get_os_i2c_factory(&args.bus)?;
-    info!("Opening I2C bus {}", &args.bus.display());
+    let factory = get_os_i2c_factory(args.bus.clone())?;
 
     // Start the looper
-    let (_sender, receiver) = channel(args.backlog);
+    let (sender, _receiver) = channel();
 
     // Display the epilogue if any
-    match tokio::spawn(weather_readouts_scheduler(factory, receiver)).await {
+    let frequency = Duration::from_secs(args.frequency as u64);
+    let driver = thread::spawn(move || weather_readouts_driver(factory, frequency, sender));
+
+    match driver.join() {
         Ok(stats) => {
             let stats = stats?;
             let uptime = Instant::now() - boot;
@@ -79,7 +97,7 @@ async fn main() -> Result<(), PiWeatherError> {
                 uptime, stats
             )
         }
-        Err(ref err) => error!("Got an error when terminating the application {}", err),
+        Err(err) => error!("Got an error when terminating the application {:?}", err),
     };
 
     Ok(())
