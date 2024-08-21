fecc594efb4cea3de01286afcfbda242d3744c6c
Morgan Funtowicz <funtowiczmo@gmail.com>
2024-08-21 13:08:30 +0000

    (scheduler) move drivers into main scheduler loop to avoid Send

--- piweather-agent/src/main.rs
+++ piweather-agent/src/main.rs
@@ -1,9 +1,10 @@
 use clap::Parser;
-use piweather_agent::i2c::get_os_i2c_factory;
+use piweather_agent::i2c::{get_os_i2c_factory, I2CDeviceFactory};
 use piweather_agent::sensors::{Am2315, Sensor};
 use piweather_common::errors::PiWeatherError;
 use piweather_common::Payload;
 use std::path::PathBuf;
+use std::time::Instant;
 use tokio::sync::mpsc::{channel, Receiver};
 use tracing::{debug, error, info};
 use tracing_subscriber;
@@ -25,7 +26,18 @@ struct Args {
     destination: String,
 }
 
-async fn weather_readouts_scheduler(mut readouts: Receiver<Payload<3>>) {
+async fn weather_readouts_scheduler<T>(
+    factory: T,
+    mut readouts: Receiver<Payload<3>>,
+) -> Result<usize, PiWeatherError>
+where
+    T: I2CDeviceFactory,
+{
+    // Initiate sensors
+    let mut am2315 = Am2315::with_i2c_factory(factory)?;
+
+    // Count the number of message sent
+    let messages = 0usize;
     loop {
         match readouts.recv().await {
             Some(payload) => {
@@ -39,6 +51,7 @@ async fn weather_readouts_scheduler(mut readouts: Receiver<Payload<3>>) {
     }
 
     info!("Scheduler exiting");
+    Ok(messages)
 }
 
 #[tokio::main(flavor = "current_thread")]
@@ -47,24 +60,27 @@ async fn main() -> Result<(), PiWeatherError> {
 
     // Parse arguments
     let args = Args::parse();
+    let boot = Instant::now();
 
     // Create the I2C bus from the provided file address
     let factory = get_os_i2c_factory(&args.bus)?;
     info!("Opening I2C bus {}", &args.bus.display());
 
-    // Initiate sensors
-    let mut am2315 = Am2315::with_i2c_factory(factory)?;
-    let readouts = am2315.read()?;
-
-    info!("Am2315 read: {:?}", readouts);
-
     // Start the looper
     let (_sender, receiver) = channel(args.backlog);
 
     // Display the epilogue if any
-    if let Err(ref err) = tokio::spawn(weather_readouts_scheduler(receiver)).await {
-        error!("Got an error when terminating the application {}", err);
-    }
+    match tokio::spawn(weather_readouts_scheduler(factory, receiver)).await {
+        Ok(stats) => {
+            let stats = stats?;
+            let uptime = Instant::now() - boot;
+            info!(
+                "Weather station exiting... Uptime {:?}, {} weather data points sent",
+                uptime, stats
+            )
+        }
+        Err(ref err) => error!("Got an error when terminating the application {}", err),
+    };
 
     Ok(())
 }
